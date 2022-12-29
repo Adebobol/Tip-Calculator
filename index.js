d205cafd47f7cd2eed70002578b57ec3c88e750e
Adebobola <adepojuadebobola6@gmail.com>
2022-12-29 20:15:23 +0100

    font family for input value

---
+++ index.js
@@ -0,0 +1,8 @@
+// "use strict"
+
+const bill = document.getElementById("#bill");
+const number = document.getElementById("#number");
+const tipBox = document.querySelectorAll(".tip-box");
+
+
+console.log("kenny")
